0d4ec05bcf155c89b921fe5cbd2e992b5e026a3a
Сиденко Кирилл Сергеевич <k.sidenko@2gis.ru>
2015-12-01 21:33:54 +0600

    add card

---
+++ Card.elm
@@ -0,0 +1,71 @@
+module Card where
+
+
+import Html exposing (..)
+import Html.Attributes exposing (style)
+import Html.Events exposing (onClick)
+
+-- MODEL
+
+type alias Model =
+    { analyticStoryPointsAvailable : Int
+    , devStoryPointsAvailable  : Int
+    , testStoryPointsAvailable  : Int
+    }
+
+init : Int -> Int -> Int -> Model
+init a d t =
+    { analyticStoryPointsAvailable = a
+    , devStoryPointsAvailable  = d
+    , testStoryPointsAvailable  = t
+    }
+
+--init : Model
+--init =
+    --{ analyticStoryPointsAvailable = 2
+    --, devStoryPointsAvailable  = 3
+    --, testStoryPointsAvailable  = 2
+    --}
+
+-- UPDATE
+type Action = Increment | Decrement | Reset
+
+update : Action -> Model -> Model
+update action model =
+  case action of
+    Reset ->
+        init 7 7 7
+
+    Increment ->
+        { model |
+            analyticStoryPointsAvailable = model.analyticStoryPointsAvailable + 1
+        }
+
+    Decrement ->
+        { model |
+            analyticStoryPointsAvailable = model.analyticStoryPointsAvailable - 1
+        }
+
+-- VIEW
+
+view : Signal.Address Action -> Model -> Html
+view address model =
+  div []
+    [ button [ onClick address Decrement ] [ text "-" ]
+    , div [ countStyle ] [ text (toString model.analyticStoryPointsAvailable ) ]
+    , div [ countStyle ] [ text (toString model.devStoryPointsAvailable) ]
+    , div [ countStyle ] [ text (toString model.testStoryPointsAvailable) ]
+    , button [ onClick address Increment ] [ text "+" ]
+    , button [ onClick address Reset ] [ text "reset" ]
+    ]
+
+
+countStyle : Attribute
+countStyle =
+  style
+    [ ("font-size", "20px")
+    , ("font-family", "monospace")
+    , ("display", "inline-block")
+    , ("width", "50px")
+    , ("text-align", "center")
+    ]
