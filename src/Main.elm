e4883bcd5e98325bfcf18fbc979e0c233e3c41c8
Fedor Nezhivoy <gyzerok@gmail.com>
2016-01-12 09:57:47 +0600

    fix typo and Main.elm

--- src/Main.elm
+++ src/Main.elm
@@ -1,17 +1,11 @@
---import Task exposing (Task, andThen)
+import StartApp.Simple as StartApp
 
---import Card exposing (init, update, view)
-import Column exposing (..)
-import ColumnGroup exposing (..)
 import Board exposing (init, update, view)
 
---https://github.com/evancz/start-app/blob/master/src/StartApp.elm
-import StartApp.Simple exposing (start)
 
 main =
-  start
+  StartApp.start
     { model = Board.init 1 2 3 4 5
     , update = Board.update
     , view = Board.view
     }
-
